49a8dee8ff76f57072d05f178a1eaa1448a6c30c
“[Monther <[monther-qasim.99@outlook.com]>
2025-05-18 11:14:37 +0300

    Launch FaunaWildSphere

---
+++ FaunaWildSphere/src/main/java/dev/springhandson/faunawildsphere/FaunaWildSphereApplication.java
@@ -0,0 +1,13 @@
+package dev.springhandson.faunawildsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaunaWildSphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaunaWildSphereApplication.class, args);
+    }
+
+}
---
+++ FaunaWildSphere/src/test/java/dev/springhandson/faunawildsphere/FaunaWildSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.springhandson.faunawildsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaunaWildSphereApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
